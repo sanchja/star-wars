1c254abaad9937930da5747a52fbf4398d342a78
JASON SANCHEZ <jasonsanc76@gmail.com>
2020-02-17 15:33:16 -0500

    Routing setup
    Created initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PeopleComponent } from '../app/components/people/people.component';
+import { StarshipsComponent } from '../app/components/starships/starships.component';
+import { PlanetsComponent } from '../app/components/planets/planets.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'people', component: PeopleComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: 'planets', component: PlanetsComponent },
+  // { path: '**', redirectTo: '/people', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
